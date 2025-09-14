fe0a1763b3723cb5dc619c42fb1445a4186dc36d
hauldhut <hauldhut@gmail.com>
2025-09-14 17:08:27 +0700

    Add files via upload

--- Code/MHMDA_KFold_Balanced_Final.R
+++ Code/MHMDA_KFold_Balanced_Final.R
@@ -1,4 +1,4 @@
-Method = "MH123_Balanced"#MH123/MH12/MH13/MH23/H1/H2/H3/H1_Balanced/H3_Balanced/MH13_Balanced
+Method = "H1_Balanced"#/H1_Balanced/H3_Balanced/MH13_Balanced
 gamma = 0.5
 
 start_time <- Sys.time()
@@ -12,113 +12,97 @@ library(doParallel)
 
 setwd("~/Manuscripts/123GNN4MDA/Code")
 
-#DiseaseSimNet_OMIM.txt
-#DiseaseSimNet_HPO.sif
-#DiseaseSimNet_GeneNet.txt
+miRNASimNet1 <- read.delim("../Data/miRNANetW.txt",header = FALSE)
+miRNASimNet1.frame <- data.frame(miRNASimNet1[[1]], miRNASimNet1[[3]])
+miRNASimNet1.g <- graph.data.frame(d = miRNASimNet1.frame, directed = FALSE)
+miRNASimNet1.weight = miRNASimNet1[[2]]
+E(miRNASimNet1.g)$weight <- miRNASimNet1.weight
 
-DiSimNet1 <- read.delim("../Data/MonoNet_miRWalk.txt",header = FALSE)
-DiSimNet1.frame <- data.frame(DiSimNet1[[1]], DiSimNet1[[3]])
-DiSimNet1.g <- graph.data.frame(d = DiSimNet1.frame, directed = FALSE)
-DiSimNet1.weight = DiSimNet1[[2]]
-E(DiSimNet1.g)$weight <- DiSimNet1.weight
+miRNASimNet2 <- read.delim("../Data/miRNANetS.txt",header = FALSE)
+miRNASimNet2.frame <- data.frame(miRNASimNet2[[1]], miRNASimNet2[[3]])
+miRNASimNet2.g <- graph.data.frame(d = miRNASimNet2.frame, directed = FALSE)
+miRNASimNet2.weight = miRNASimNet2[[2]]
+E(miRNASimNet2.g)$weight <- miRNASimNet2.weight
 
-DiSimNet2 <- read.delim("../Data/MonoNet_TargetScan.txt",header = FALSE)
-DiSimNet2.frame <- data.frame(DiSimNet2[[1]], DiSimNet2[[3]])
-DiSimNet2.g <- graph.data.frame(d = DiSimNet2.frame, directed = FALSE)
-DiSimNet2.weight = DiSimNet2[[2]]
-E(DiSimNet2.g)$weight <- DiSimNet2.weight
+miRNASimNet3 <- read.delim("../Data/miRNANetB.txt",header = FALSE)
+miRNASimNet3.frame <- data.frame(miRNASimNet3[[1]], miRNASimNet3[[3]])
+miRNASimNet3.g <- graph.data.frame(d = miRNASimNet3.frame, directed = FALSE)
+miRNASimNet3.weight = miRNASimNet3[[2]]
+E(miRNASimNet3.g)$weight <- miRNASimNet3.weight
 
-DiSimNet3 <- read.delim("../Data/MonoNet_miRTarBase.txt",header = FALSE)
-DiSimNet3.frame <- data.frame(DiSimNet3[[1]], DiSimNet3[[3]])
-DiSimNet3.g <- graph.data.frame(d = DiSimNet3.frame, directed = FALSE)
-DiSimNet3.weight = DiSimNet3[[2]]
-E(DiSimNet3.g)$weight <- DiSimNet3.weight
-
-if(Method == "MH123"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet1.g,DiSimNet2.g,DiSimNet3.g),Layers_Name = c("DiSimNet1","DiSimNet2","DiSimNet3"))  
+if(Method == "MH123_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet1.g,miRNASimNet2.g,miRNASimNet3.g),Layers_Name = c("miRNASimNet1","miRNASimNet2","miRNASimNet3"))  
   tau1 = 1
   tau2 = 1
   tau3 = 1
   tau <- c(tau1, tau2, tau3)
-}else if(Method == "MH12"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet1.g,DiSimNet2.g),Layers_Name = c("DiSimNet1","DiSimNet2"))
+}else if(Method == "MH12_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet1.g,miRNASimNet2.g),Layers_Name = c("miRNASimNet1","miRNASimNet2"))
   tau1 = 1
   tau2 = 1
   tau <- c(tau1, tau2)
-}else if(Method == "MH13"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet1.g,DiSimNet3.g),Layers_Name = c("DiSimNet1","DiSimNet3"))
+}else if(Method == "MH13_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet1.g,miRNASimNet3.g),Layers_Name = c("miRNASimNet1","miRNASimNet3"))
   tau1 = 1
   tau2 = 1
   tau <- c(tau1, tau2)
-}else if(Method == "MH23"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet2.g,DiSimNet3.g),Layers_Name = c("DiSimNet2","DiSimNet3"))
+}else if(Method == "MH23_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet2.g,miRNASimNet3.g),Layers_Name = c("miRNASimNet2","miRNASimNet3"))
   tau1 = 1
   tau2 = 1
   tau <- c(tau1, tau2)
-}else if(Method == "H1"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet1.g),Layers_Name = c("DiSimNet1"))
+}else if(Method == "H1_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet1.g),Layers_Name = c("miRNASimNet1"))
   tau <- c(1)
-}else if(Method == "H2"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet2.g),Layers_Name = c("DiSimNet2"))
+}else if(Method == "H2_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet2.g),Layers_Name = c("miRNASimNet2"))
   tau <- c(1)
-}else if(Method == "H3"){
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet3.g),Layers_Name = c("DiSimNet3"))
+}else if(Method == "H3_Balanced"){
+  miRNA_MultiplexObject <- create.multiplex(list(miRNASimNet3.g),Layers_Name = c("miRNASimNet3"))
   tau <- c(1)
-}else{ #H1_Balanced/H3_Balanced/MH13_Balanced/MH123_Balanced
-  disease_MultiplexObject <- create.multiplex(list(DiSimNet1.g,DiSimNet2.g,DiSimNet3.g),Layers_Name = c("DiSimNet1","DiSimNet2","DiSimNet3"))  
-  tau1 = 1
-  tau2 = 1
-  tau3 = 1
-  tau <- c(tau1, tau2, tau3)
 }
 
-#DrugSimNet_PREDICT.txt
-#DrugSimNet_CHEM.txt
-DrugSimNet = "MeSHUI"
-if(DrugSimNet == "MeSHUI"){
-  DrSimNet <- read.delim("../Data/MeSHUI_Net.txt",header = FALSE)
-}else{
-  DrSimNet <- read.delim("../Data/DrugSimNet_CHEM.txt",header = FALSE)  
-}
+DiSimNet <- read.delim("../Data/DiseaseSimNet_OMIM.txt",header = FALSE)  
 
-DrSimNet.frame <- data.frame(DrSimNet[[1]], DrSimNet[[3]])
-DrSimNet.weight = DrSimNet[[2]]
 
-DrSimNet.g <- graph.data.frame(d = DrSimNet.frame, directed = FALSE)
-E(DrSimNet.g)$weight <- DrSimNet.weight
+DiSimNet.frame <- data.frame(DiSimNet[[1]], DiSimNet[[3]])
+DiSimNet.weight = DiSimNet[[2]]
 
-drug_MultiplexObject <- create.multiplex(list(DrSimNet.g),
-                                         Layers_Name = c("DrSimNet"))
+DiSimNet.g <- graph.data.frame(d = DiSimNet.frame, directed = FALSE)
+E(DiSimNet.g)$weight <- DiSimNet.weight
 
-#Add DiDrRelation
-DiDr.frame <- read.csv("../Data/miRNA2Disease_HMDD_MeSHUI_final.csv", header = TRUE)
-DiDr.frame <- DiDr.frame[which(DiDr.frame$miRNA %in% disease_MultiplexObject$Pool_of_Nodes),]
-DiDr.frame <- DiDr.frame[which(DiDr.frame$disease %in% drug_MultiplexObject$Pool_of_Nodes),]
+disease_MultiplexObject <- create.multiplex(list(DiSimNet.g),
+                                         Layers_Name = c("DiSimNet"))
+
+#Add miDiRelation
+miDi.frame <- read.csv("../Data/Phenotype2miRNAs_HMDD.csv", header = TRUE)
+miDi.frame <- miDi.frame[which(miDi.frame$miRNA %in% miRNA_MultiplexObject$Pool_of_Nodes),]
+miDi.frame <- miDi.frame[which(miDi.frame$disease %in% disease_MultiplexObject$Pool_of_Nodes),]
 
 #add func for RWR on multiplex-heter nw
-do_something <- function(disease_MultiplexObject,drug_MultiplexObject,
-                         DiDrRelation,SeedDisease, seeddrug, prd_diseases) {
+do_something <- function(miRNA_MultiplexObject,disease_MultiplexObject,
+                         miDiRelation,SeedmiRNA, seeddisease, prd_miRNAs) {
   
   #Create multiplex-heterosgenous nw
-  DiDrRelation_disease <- DiDrRelation[which(DiDrRelation$miRNA %in% disease_MultiplexObject$Pool_of_Nodes),]
+  miDiRelation_miRNA <- miDiRelation[which(miDiRelation$miRNA %in% miRNA_MultiplexObject$Pool_of_Nodes),]
   
   #Create multiplex-heterosgenous nw
-  disease_drug_Net <- create.multiplexHet(disease_MultiplexObject, drug_MultiplexObject, 
-                                          DiDrRelation_disease)
+  miRNA_disease_Net <- create.multiplexHet(miRNA_MultiplexObject, disease_MultiplexObject, 
+                                          miDiRelation_miRNA)
   
-  disease_drug_Net_TranMatrix <- compute.transition.matrix(disease_drug_Net)
+  miRNA_disease_Net_TranMatrix <- compute.transition.matrix(miRNA_disease_Net)
   
   #compute 
-  Ranking_Results <- Random.Walk.Restart.MultiplexHet(disease_drug_Net_TranMatrix,
-                                                      disease_drug_Net,SeedDisease,
-                                                      seeddrug, r = gamma)
+  Ranking_Results <- Random.Walk.Restart.MultiplexHet(miRNA_disease_Net_TranMatrix,
+                                                      miRNA_disease_Net,SeedmiRNA,
+                                                      seeddisease, r = gamma)
   
   #create labels for ranking results
   tf = Ranking_Results$RWRMH_Multiplex1
   
-  tf$labels <- ifelse(tf$NodeNames %in% prd_diseases, 1, 0)
+  tf$labels <- ifelse(tf$NodeNames %in% prd_miRNAs, 1, 0)
   
-  # Select all nodes with label=1 and equal number of random nodes with label=0
+  #Balanced: Select all nodes with label=1 and equal number of random nodes with label=0
   label_1_indices <- which(tf$labels == 1)
   label_0_indices <- which(tf$labels == 0)
   n_label_1 <- length(label_1_indices)
@@ -135,25 +119,25 @@ do_something <- function(disease_MultiplexObject,drug_MultiplexObject,
   return(list(pauc.perf@y.values[[1]],data.frame(Scores=tf$Score, Labels=tf$labels)))
 }
 
-#count disease for each drug
-sub_sum <- aggregate(miRNA~disease, data=DiDr.frame, FUN=function(x) c(count=length(x)))
+#count miRNA for each disease
+sub_sum <- aggregate(miRNA~disease, data=miDi.frame, FUN=function(x) c(count=length(x)))
 
-#extract drug with only k or more diseases
+#extract disease with only k or more miRNAs
 k=5
 sub_sum <- sub_sum[which(sub_sum$miRNA>=k),]
-sub_sum$disease_no <- c(1:length(sub_sum$disease))
+sub_sum$disease_no <- c(1:length(sub_sum$miRNA))
 
-#extract DiDr.frame with only disease from sub_sum
-DiDr.frame1 <- DiDr.frame[which(DiDr.frame$disease %in% sub_sum$disease),]
-rownames(DiDr.frame1) <- NULL #reset frame index
+#extract miDi.frame with only disease from sub_sum
+miDi.frame1 <- miDi.frame[which(miDi.frame$disease %in% sub_sum$disease),]
+rownames(miDi.frame1) <- NULL #reset frame index
 
-#func to assign k groups for each set of drug-disease, 
+#func to assign k groups for each set of disease-miRNA, 
 #as well as increment group no. for each group (k=3)
-assign_group_no <- function(sub_sum,DiDr.frame1,k) {
+assign_group_no <- function(sub_sum,miDi.frame1,k) {
   
-  #set an empty data frame for a new DiDr.frame
+  #set an empty data frame for a new miDi.frame
   mylist.names <- c("disease","miRNA", "disease_no","group_no")
-  DiDr.frame2 <- sapply(mylist.names,function(x) NULL)
+  miDi.frame2 <- sapply(mylist.names,function(x) NULL)
   
   for (j in 1:length(sub_sum$disease)) {
     
@@ -171,26 +155,26 @@ assign_group_no <- function(sub_sum,DiDr.frame1,k) {
     }
     group_vec = c(group_vec,rep(k,count-set_no*(k-1)))
     
-    subset <- DiDr.frame[which(DiDr.frame$disease==sub_sum$disease[[j]]),]
+    subset <- miDi.frame[which(miDi.frame$disease==sub_sum$disease[[j]]),]
     subset$disease_no <- rep(j,count)
     subset$group <- group_vec
     
-    DiDr.frame2 <- rbind(DiDr.frame2,subset)
+    miDi.frame2 <- rbind(miDi.frame2,subset)
   }
-  return(DiDr.frame2)
+  return(miDi.frame2)
 }
 
-#assign each group of n/k disease-drug with an group id
-out <- assign_group_no(sub_sum,DiDr.frame1,k)
+#assign each group of n/k miRNA-disease with an group id
+out <- assign_group_no(sub_sum,miDi.frame1,k)
 out$group <- (out$disease_no-1)*k+out$group
-DiDr.frame2 <- out
+miDi.frame2 <- out
 
-#set an empty data frame for a new DiDr.frame
+#set an empty data frame for a new miDi.frame
 auc_results <- sapply(c("disease","group","auc"),function(x) NULL)
 
-for (i in 1:max(DiDr.frame2$group)) {
-  seeddrug = unique(DiDr.frame2$disease[which(DiDr.frame2$group==i)])
-  auc_results$disease[i] <- seeddrug
+for (i in 1:max(miDi.frame2$group)) {
+  seeddisease = unique(miDi.frame2$disease[which(miDi.frame2$group==i)])
+  auc_results$disease[i] <- seeddisease
   auc_results$group[i] <- i
 }
 
@@ -200,34 +184,34 @@ cl <- makeCluster(no_cores)
 registerDoParallel(cl)
 
 #loop through
-res <- foreach(i = 1:max(DiDr.frame2$group), .combine = rbind) %dopar% {
+res <- foreach(i = 1:max(miDi.frame2$group), .combine = rbind) %dopar% {
   
   library(RandomWalkRestartMH)
   library(igraph)
   library(ROCR)
   
-  prd_diseases = DiDr.frame2$miRNA[which(DiDr.frame2$group==i)]
-  seeddrug = unique(DiDr.frame2$disease[which(DiDr.frame2$group==i)])
+  prd_miRNAs = miDi.frame2$miRNA[which(miDi.frame2$group==i)]
+  seeddisease = unique(miDi.frame2$disease[which(miDi.frame2$group==i)])
   
-  disease_relation = DiDr.frame2[which(DiDr.frame2$disease==seeddrug),]
-  SeedDisease = disease_relation$miRNA[-c(which(disease_relation$miRNA %in% prd_diseases))]
+  disease_relation = miDi.frame2[which(miDi.frame2$disease==seeddisease),]
+  SeedmiRNA = disease_relation$miRNA[-c(which(disease_relation$miRNA %in% prd_miRNAs))]
   
-  # get bipartite graph without prd_diseases - disease linkages
-  DiDrRelation <- DiDr.frame2[-with(DiDr.frame2, which(miRNA %in% prd_diseases & disease %in% seeddrug)),][1:2]
+  # get bipartite graph without prd_miRNAs - disease linkages
+  miDiRelation <- miDi.frame2[-with(miDi.frame2, which(miRNA %in% prd_miRNAs & disease %in% seeddisease)),c("miRNA","disease")]
   
-  res <- do_something(disease_MultiplexObject,drug_MultiplexObject,
-                      DiDrRelation,SeedDisease, seeddrug, prd_diseases)
-  res <- append(res, seeddrug)
+  res <- do_something(miRNA_MultiplexObject,disease_MultiplexObject,
+                      miDiRelation,SeedmiRNA, seeddisease, prd_miRNAs)
+  res <- append(res, seeddisease)
 }
 
 dim(res)
 stopCluster(cl)
 
 df.res = data.frame(trial = c(1:nrow(res)),auc = unlist(res[,1]))
-df.res.final = merge(df.res, DiDr.frame2[c("disease","group")], by.x="trial", by.y="group", all.x = TRUE)
+df.res.final = merge(df.res, miDi.frame2[c("disease","group")], by.x="trial", by.y="group", all.x = TRUE)
 df.res.final = unique(df.res.final)
 
-Result_byTrialFile = paste0("../Results/",Method,"_byTrial_",DrugSimNet,"_ROC_KFold10.csv")
+Result_byTrialFile = paste0("../Results/",Method,"_byTrial_",DiseaseSimNet,"_ROC_KFold",k,".csv")
 cat(Result_byTrialFile,"\n")
 write.csv(df.res.final,Result_byTrialFile, row.names = FALSE, quote = FALSE)
 
@@ -239,7 +223,7 @@ for(i in 1:nrow(res)){
   res.final = rbind(res.final, res[i,2][[1]])
 }
 
-Result_by_Score_n_LabelFile = paste0("../Results/",Method,"_byTrial_",DrugSimNet,"_Score_n_Label_KFold10.csv")
+Result_by_Score_n_LabelFile = paste0("../Results/",Method,"_byTrial_",DiseaseSimNet,"_Score_n_Label_KFold",k,".csv")
 cat(Result_by_Score_n_LabelFile,"\n")
 write.csv(res.final,Result_by_Score_n_LabelFile, row.names = FALSE, quote = FALSE)
 
@@ -248,7 +232,7 @@ resultspred = prediction(res.final$Scores, res.final$Labels)
 auc.perf = performance(resultspred, measure = "auc")
 aucavgbyAll = round(auc.perf@y.values[[1]],3)
 
-cat("Method=",Method,'DrugSimNet=',DrugSimNet,'aucavgbyAll=',aucavgbyAll,'aucavgbyTrial=',aucavgbyTrial,"(+-",aucavgbyTrial.sd,")\n")
+cat("Method=",Method,'DiseaseSimNet=',DiseaseSimNet,'aucavgbyAll=',aucavgbyAll,'aucavgbyTrial=',aucavgbyTrial,"(+-",aucavgbyTrial.sd,")\n")
 
 end_time <- Sys.time()
 timediff = end_time - start_time
